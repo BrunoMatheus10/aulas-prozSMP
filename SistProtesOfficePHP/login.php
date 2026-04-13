11d5c5ed286596de8a49b9001f69b0a38aac3e6d
Bruno Matheus <brunomatheus88300432@gmail.com>
2026-04-13 09:02:53 -0300

    Add files via upload

---
+++ SistProtesOfficePHP/login.php
@@ -0,0 +1,18 @@
+<link rel="stylesheet" type="text/css" href="stylet.css">
+<?php
+
+
+$login = "bruno";
+$senha = "112233";
+
+if ($_POST["login"] == $login and $_POST["senha"] == $senha) {
+    echo "<h4> seja bem-vindo(a), $login </h4>";
+    header (header: "Refresh: 2; URL=office.html");
+}
+else {
+    echo "<h4> Login ou senha invalidos! </h4>";
+    header (header: "refresh: 2; URL=index.html");
+}
+
+?>
+
